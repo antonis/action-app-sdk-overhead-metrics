3ccd62f6d212014ab9dc23a4c5e3dddb67f655bf
Antonis Lilis <antonis.lilis@gmail.com>
2024-11-21 11:10:57 +0200

    Parse app ready timestamp from log

--- src/test/kotlin/StartupTimeTest.kt
+++ src/test/kotlin/StartupTimeTest.kt
@@ -203,6 +203,8 @@ class StartupTimeTest : TestBase() {
                 printf("%s", "${app.name} is installed: ${driver.isAppInstalled(app.name)}")
                 driver.terminateApp(app.name)
 
+                val startTime = System.currentTimeMillis()
+
                 try {
                     val result = androidDriver.executeScript(
                         "mobile: startActivity",
@@ -220,25 +222,50 @@ class StartupTimeTest : TestBase() {
                 }
 
                 val logEntries = driver.manage().logs().get("logcat")
-                val regex = Regex("Displayed ${app.name}/\\.${app.activity}: \\+(?:([0-9]+)s)?([0-9]+)ms")
-                val times = logEntries.mapNotNull {
-                    val groups = regex.find(it.message)?.groupValues
-                    if (groups == null) {
-                        null
-                    } else {
-                        printf("$logAppPrefix logcat entry processed: %s", app.name, it.message)
-                        val seconds = if (groups[1].isEmpty()) 0 else groups[1].toLong()
-                        seconds * 1000 + groups[2].toLong()
+                if (app.logLoadedTimestampRegex != null) {
+                    val regex = Regex(app.logLoadedTimestampRegex)
+                    val times = logEntries.mapNotNull {
+                        val groups = regex.find(it.message)?.groupValues
+                        if (groups == null) {
+                            null
+                        } else {
+                            printf("$logAppPrefix logcat entry processed: %s", app.name, it.message)
+                            groups[1].toLong()
+                        }
                     }
-                }
-                if (times.size == 1) {
-                    appTimes.add(times.first())
+                    if (times.size == 1) {
+                        val loadTime = times.first() - startTime
+                        appTimes.add(loadTime)
+                        printf("$logAppPrefix loaded timestamp: %d", app.name, loadTime)
+                    } else {
+                        printf(
+                            "Expected 1 loaded timestamp in logcat, matching Regex '%s', but found %d",
+                            regex.pattern,
+                            times.size
+                        )
+                    }
                 } else {
-                    printf(
-                        "Expected 1 startup time in logcat, matching Regex '%s', but found %d",
-                        regex.pattern,
-                        times.size
-                    )
+                    val regex =
+                        Regex("Displayed ${app.name}/\\.${app.activity}: \\+(?:([0-9]+)s)?([0-9]+)ms")
+                    val times = logEntries.mapNotNull {
+                        val groups = regex.find(it.message)?.groupValues
+                        if (groups == null) {
+                            null
+                        } else {
+                            printf("$logAppPrefix logcat entry processed: %s", app.name, it.message)
+                            val seconds = if (groups[1].isEmpty()) 0 else groups[1].toLong()
+                            seconds * 1000 + groups[2].toLong()
+                        }
+                    }
+                    if (times.size == 1) {
+                        appTimes.add(times.first())
+                    } else {
+                        printf(
+                            "Expected 1 startup time in logcat, matching Regex '%s', but found %d",
+                            regex.pattern,
+                            times.size
+                        )
+                    }
                 }
             }
 
